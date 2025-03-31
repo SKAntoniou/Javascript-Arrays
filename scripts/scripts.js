8e013de3407dbfdbf1ab0db1cd2014dfc4c7283a
SKAntoniou <SKAntoniou@gmail.com>
2025-03-31 14:33:52 +0100

    generator with loads of bugs. redoing now with different method

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,4 @@
-// Light / Dark Mode Toggle
+// Light / Dark Mode Toggle ===================================================
 const lightDarkToggle = document.getElementById('light-dark-toggle');
 lightDarkToggle.addEventListener("change", () => {
   for (let currentValue of lightDarkToggle.children) {
@@ -17,3 +17,77 @@ lightDarkToggle.addEventListener("change", () => {
   }
 });
 
+// Fetch Functions ===================================================
+function fetchData(url) {
+  return fetch(url)
+    .then(checkStatus())
+    .then(res => res.json())
+    .catch(error => console.log(`Failed to fetch an image from ${url} \n`, error))
+}
+function checkStatus(response) {
+  if (response.ok) {
+    return Promise.resolve(response);
+  } else {
+    return Promise.reject(new Error(response.statusText));
+  }
+}
+
+// Random image fetch ===================================================
+// Parent containers
+const imagePreviewContainer = document.querySelector(".image-preview");
+const userArea = document.querySelector(".user-area");
+
+// Child elements
+const genImageContainer = imagePreviewContainer.querySelector(".generated-image");
+const newImageButton = imagePreviewContainer.querySelector(".btn-refresh");
+const saveImageButton = imagePreviewContainer.querySelector(".btn-save");
+const userSavedImages = userArea.querySelector(".user-saved-images");
+
+const testImg = userSavedImages.querySelector(".test-img");
+
+// When page loads - Add a random image.
+genImageContainer.src = "https://picsum.photos/600/400?random=1";
+genImageContainer.crossOrigin = "anonymous";
+
+
+// genImageContainer.addEventListener('load', () => {
+
+  newImageButton.addEventListener('click', () => {
+    genImageContainer.src = "https://picsum.photos/600/400?random=1";
+    genImageContainer.crossOrigin = "anonymous";
+  })
+
+  saveImageButton.addEventListener('click', () => {
+    // genImageContainer.crossOrigin = "anonymous";
+    const imgBase64Data = getBase64Image(genImageContainer);
+    localStorage.setItem("imgData",imgBase64Data);
+  
+    let imgBase64DataCopy = localStorage.getItem('imgData');
+    imgBase64DataCopy = "data:image/png;base64," + imgBase64DataCopy;
+
+    userSavedImages.appendChild( createImgContainer(imgBase64DataCopy) );
+  })
+// })
+
+// OLD method, maybe cause of clipping, trying blob now
+function getBase64Image(img) {
+  let canvas = document.createElement("canvas");
+  canvas.width = img.width;
+  canvas.height = img.height;
+
+  let ctx = canvas.getContext("2d");
+  ctx.drawImage(img, 0, 0);
+
+  let dataURL = canvas.toDataURL("image/png");
+
+  return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+}
+
+
+function createImgContainer(imageData) {
+  const htmlElement = document.createElement("img");
+  htmlElement.classList.add("img");
+  htmlElement.alt = "Random Image";
+  htmlElement.src = imageData;
+  return htmlElement;
+}
